d81550a6cc954aa54ec949a457cf12e15ee84e60
xwhqsj <xwhqsj@gmail.com>
2016-12-09 17:14:29 +0800

    add more files

---
+++ BmpToJpg/BmpToJpgDlg.cpp
@@ -0,0 +1,208 @@
+// BmpToJpgDlg.cpp : implementation file
+//
+
+#include "stdafx.h"
+#include "BmpToJpg.h"
+#include "BmpToJpgDlg.h"
+
+#ifdef _DEBUG
+#define new DEBUG_NEW
+#undef THIS_FILE
+static char THIS_FILE[] = __FILE__;
+#endif
+
+/////////////////////////////////////////////////////////////////////////////
+// CAboutDlg dialog used for App About
+
+class CAboutDlg : public CDialog
+{
+public:
+	CAboutDlg();
+
+// Dialog Data
+	//{{AFX_DATA(CAboutDlg)
+	enum { IDD = IDD_ABOUTBOX };
+	//}}AFX_DATA
+
+	// ClassWizard generated virtual function overrides
+	//{{AFX_VIRTUAL(CAboutDlg)
+	protected:
+	virtual void DoDataExchange(CDataExchange* pDX);    // DDX/DDV support
+	//}}AFX_VIRTUAL
+
+// Implementation
+protected:
+	//{{AFX_MSG(CAboutDlg)
+	//}}AFX_MSG
+	DECLARE_MESSAGE_MAP()
+};
+
+CAboutDlg::CAboutDlg() : CDialog(CAboutDlg::IDD)
+{
+	//{{AFX_DATA_INIT(CAboutDlg)
+	//}}AFX_DATA_INIT
+}
+
+void CAboutDlg::DoDataExchange(CDataExchange* pDX)
+{
+	CDialog::DoDataExchange(pDX);
+	//{{AFX_DATA_MAP(CAboutDlg)
+	//}}AFX_DATA_MAP
+}
+
+BEGIN_MESSAGE_MAP(CAboutDlg, CDialog)
+	//{{AFX_MSG_MAP(CAboutDlg)
+		// No message handlers
+	//}}AFX_MSG_MAP
+END_MESSAGE_MAP()
+
+/////////////////////////////////////////////////////////////////////////////
+// CBmpToJpgDlg dialog
+
+CBmpToJpgDlg::CBmpToJpgDlg(CWnd* pParent /*=NULL*/)
+	: CDialog(CBmpToJpgDlg::IDD, pParent)
+{
+	//{{AFX_DATA_INIT(CBmpToJpgDlg)
+	m_strBmp = _T("");
+	m_strJpg = _T("");
+	//}}AFX_DATA_INIT
+	// Note that LoadIcon does not require a subsequent DestroyIcon in Win32
+	m_hIcon = AfxGetApp()->LoadIcon(IDR_MAINFRAME);
+}
+
+void CBmpToJpgDlg::DoDataExchange(CDataExchange* pDX)
+{
+	CDialog::DoDataExchange(pDX);
+	//{{AFX_DATA_MAP(CBmpToJpgDlg)
+	DDX_Text(pDX, IDC_EDIT1, m_strBmp);
+	DDX_Text(pDX, IDC_EDIT2, m_strJpg);
+	//}}AFX_DATA_MAP
+}
+
+BEGIN_MESSAGE_MAP(CBmpToJpgDlg, CDialog)
+	//{{AFX_MSG_MAP(CBmpToJpgDlg)
+	ON_WM_SYSCOMMAND()
+	ON_WM_PAINT()
+	ON_WM_QUERYDRAGICON()
+	ON_BN_CLICKED(IDC_BUTTON1, OnButton1)
+	ON_BN_CLICKED(IDC_BUTTON2, OnButton2)
+	ON_BN_CLICKED(IDC_BUTTON3, OnButton3)
+	//}}AFX_MSG_MAP
+END_MESSAGE_MAP()
+
+/////////////////////////////////////////////////////////////////////////////
+// CBmpToJpgDlg message handlers
+
+BOOL CBmpToJpgDlg::OnInitDialog()
+{
+	CDialog::OnInitDialog();
+
+	// Add "About..." menu item to system menu.
+
+	// IDM_ABOUTBOX must be in the system command range.
+	ASSERT((IDM_ABOUTBOX & 0xFFF0) == IDM_ABOUTBOX);
+	ASSERT(IDM_ABOUTBOX < 0xF000);
+
+	CMenu* pSysMenu = GetSystemMenu(FALSE);
+	if (pSysMenu != NULL)
+	{
+		CString strAboutMenu;
+		strAboutMenu.LoadString(IDS_ABOUTBOX);
+		if (!strAboutMenu.IsEmpty())
+		{
+			pSysMenu->AppendMenu(MF_SEPARATOR);
+			pSysMenu->AppendMenu(MF_STRING, IDM_ABOUTBOX, strAboutMenu);
+		}
+	}
+
+	// Set the icon for this dialog.  The framework does this automatically
+	//  when the application's main window is not a dialog
+	SetIcon(m_hIcon, TRUE);			// Set big icon
+	SetIcon(m_hIcon, FALSE);		// Set small icon
+	
+	// TODO: Add extra initialization here
+	
+	return TRUE;  // return TRUE  unless you set the focus to a control
+}
+
+void CBmpToJpgDlg::OnSysCommand(UINT nID, LPARAM lParam)
+{
+	if ((nID & 0xFFF0) == IDM_ABOUTBOX)
+	{
+		CAboutDlg dlgAbout;
+		dlgAbout.DoModal();
+	}
+	else
+	{
+		CDialog::OnSysCommand(nID, lParam);
+	}
+}
+
+// If you add a minimize button to your dialog, you will need the code below
+//  to draw the icon.  For MFC applications using the document/view model,
+//  this is automatically done for you by the framework.
+
+void CBmpToJpgDlg::OnPaint() 
+{
+	if (IsIconic())
+	{
+		CPaintDC dc(this); // device context for painting
+
+		SendMessage(WM_ICONERASEBKGND, (WPARAM) dc.GetSafeHdc(), 0);
+
+		// Center icon in client rectangle
+		int cxIcon = GetSystemMetrics(SM_CXICON);
+		int cyIcon = GetSystemMetrics(SM_CYICON);
+		CRect rect;
+		GetClientRect(&rect);
+		int x = (rect.Width() - cxIcon + 1) / 2;
+		int y = (rect.Height() - cyIcon + 1) / 2;
+
+		// Draw the icon
+		dc.DrawIcon(x, y, m_hIcon);
+	}
+	else
+	{
+		CDialog::OnPaint();
+	}
+}
+
+// The system calls this to obtain the cursor to display while the user drags
+//  the minimized window.
+HCURSOR CBmpToJpgDlg::OnQueryDragIcon()
+{
+	return (HCURSOR) m_hIcon;
+}
+
+typedef  bool(__stdcall CHANGE)(char *,char *);
+
+void CBmpToJpgDlg::OnButton1() 
+{
+	UpdateData(TRUE);
+	HINSTANCE hInst;
+	CHANGE *pFunc;/*/D:\\Program Files\\Borland\\CBuilder6\\Projects\\*/
+	hInst = ::LoadLibrary("jpgdll.dll");
+	pFunc = (CHANGE*)::GetProcAddress(hInst,"BmpToJpg");
+	pFunc(m_strBmp.GetBuffer(50),m_strJpg.GetBuffer(50));
+	m_strBmp.ReleaseBuffer();
+	m_strJpg.ReleaseBuffer();
+	::FreeLibrary(hInst);
+}
+
+void CBmpToJpgDlg::OnButton2() 
+{
+	CFileDialog dlg(TRUE,"bmp",NULL,OFN_HIDEREADONLY |OFN_OVERWRITEPROMPT,"Bmp File(*.bmp)|*.bmp||");
+	if(dlg.DoModal()==IDOK){
+		m_strBmp = dlg.GetPathName();
+		UpdateData(FALSE);
+	}
+}
+
+void CBmpToJpgDlg::OnButton3() 
+{
+	CFileDialog dlg(FALSE,"jpg",NULL,OFN_HIDEREADONLY |OFN_OVERWRITEPROMPT,"Jpg File(*.jpg)|*.jpg||");
+	if(dlg.DoModal()==IDOK){
+		m_strJpg = dlg.GetPathName();
+		UpdateData(FALSE);
+	}
+}
---
+++ JpgToBmp/BmpToJpgDlg.h
@@ -0,0 +1,67 @@
+// BmpToJpgDlg.h : header file
+//
+
+#include "StdAfx.h"
+#include "BmpToJpg.h"
+#include "JpgToBmp.h"
+
+#if !defined(AFX_BMPTOJPGDLG_H__B4783D11_FCD2_4F0D_8988_AEB3F635DF97__INCLUDED_)
+#define AFX_BMPTOJPGDLG_H__B4783D11_FCD2_4F0D_8988_AEB3F635DF97__INCLUDED_
+
+#if _MSC_VER > 1000
+#pragma once
+#endif // _MSC_VER > 1000
+
+/////////////////////////////////////////////////////////////////////////////
+// CBmpToJpgDlg dialog
+
+class CBmpToJpgDlg : public CDialog
+{
+// Construction
+public:
+	CBmpToJpgDlg(CWnd* pParent = NULL);	// standard constructor
+
+// Dialog Data
+	//{{AFX_DATA(CBmpToJpgDlg)
+	enum { IDD = IDD_BMPTOJPG_DIALOG };
+
+	//}}AFX_DATA
+
+	// ClassWizard generated virtual function overrides
+	//{{AFX_VIRTUAL(CBmpToJpgDlg)
+	protected:
+	virtual void DoDataExchange(CDataExchange* pDX);	// DDX/DDV support
+	//}}AFX_VIRTUAL
+
+// Implementation
+protected:
+	HICON m_hIcon;
+
+	// Generated message map functions
+	//{{AFX_MSG(CBmpToJpgDlg)
+	virtual BOOL OnInitDialog();
+	afx_msg void OnSysCommand(UINT nID, LPARAM lParam);
+	afx_msg void OnPaint();
+	afx_msg HCURSOR OnQueryDragIcon();
+	//}}AFX_MSG
+	DECLARE_MESSAGE_MAP()
+
+public:
+	afx_msg void OnBnClickedButtonChange();
+	afx_msg void OnBnClickedButtonStrJpg();
+	afx_msg void OnBnClickedButtonStrBmp();
+	afx_msg void OnBnClickedCancel();
+
+public:
+	CEdit m_editFilePath;
+	CEdit m_editOutFilePath;
+
+	CString m_strJpg;
+	CString m_strBmp;
+	CJpgToBmp	m_jpgToBmp;
+};
+
+//{{AFX_INSERT_LOCATION}}
+// Microsoft Visual C++ will insert additional declarations immediately before the previous line.
+
+#endif // !defined(AFX_BMPTOJPGDLG_H__B4783D11_FCD2_4F0D_8988_AEB3F635DF97__INCLUDED_)
